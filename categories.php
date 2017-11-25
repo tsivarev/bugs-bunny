8a987dd711597c7f207d73f339658126192706eb
akonstantinov <a.konstantinov@corp.vk.com>
2017-11-25 16:26:19 +0300

    skills config

---
+++ categories.php
@@ -0,0 +1,365 @@
+<?php
+
+$info = <<<EOF
+	2, (3330),  Specialists									
+		21, (1081),  Specialists in natural sciences and engineering						Technical skills	Higher Education	Low Speaking
+			214, (669),  Specialists in technology (eg electrical technology							
+				2141, (60),  Industrial Manufacturing and Production Engineering.						
+				2142, (214),  Civil Engineering Specialists						
+				2143, (11),  Specialists in environmental technology						
+				2144, (228),  Specialists in Mechanical Engineering						
+				2145, (25),  Wood Processing and Chemical Process Engineering						
+				2149, (163),  Other technical specialists						
+			215, (313),  Specialists in electrical technology							
+				2151, (219),  Specialists in Electrical Engineering						
+				2152, (47),  Specialists in Electronics						
+				2153, (58),  ICT specialists						
+			216, (67),  architects							
+				2161, (27),  Architects of Building Construction						
+				2164, (10),  Urban and Traffic Designers						
+				2165, (21),  Surveying and surveying.						
+		22, (314),  Specialists in Health Care						Health care	Higher Education	Low Speaking
+			221, (145),  Doctors					High Speaking		
+				2211, (61),  general practitioners						
+				2212, (84),  surgeons and specialists						
+			222, (16),  Specialists in Nursing							
+				2221, (16),  Surgeons and nurses						
+			225, (14),  Veterinarians							
+				2250, (14),  Veterinarians				Animals		
+			226, (139),  Other Specialists in Health Care					High Speaking		
+				2261, (47),  Dentists						
+				2262, (3),  Physician						
+				2263, (4),  Environmental Health and Safety at Work.						
+				2265, (6),  Specialists in nutrition						
+				2266, (67),  Speech Therapists and Speech Therapists						
+				2269, (12),  Other unclassified health care er.						
+		23, (591),  Teachers and other specialists in the field of education						High Speaking	High level intelligence	Help others
+			231, (77),  University and college teachers							
+				2310, (77),  Teachers of Universities and Polytechnics						
+			232, (36),  Vocational Education Teachers							
+				2320, (36),  Vocational Education Teachers						
+			233, (91),  upper secondary school teachers and primary school teachers							
+				2330, (91),  upper secondary school teachers and elementary school teachers						
+			234, (304),  Primary school classes							
+				2341, (69),  Primary School Teachers						
+				2342, (237),  nursery teachers						
+			235, (92),  Other specialists in the field of education							
+				2351, (16),  Specialists in Teaching Methods						
+				2352, (33),  Special Teachers						
+				2353, (8),  Other language teachers						
+				2354, (3),  Other music teachers						
+				2355, (3),  Other Teachers of Art Education						
+				2356, (2),  Other IT teachers and trainers						
+				2359, (28),  study counselors and other teaching er.						
+		24, (478),  Specialists in Business and Administration								
+			241, (57),  Financial experts					High level intelligence		
+				2411, (39),  Accounting er. and auditors						
+				2412, (10),  Financial and Investment Advisers						
+				2413, (8),  Financial analysts						
+			242, (99),  Special Administrative Advisers				High Speaking	High level intelligence		
+				2421, (7),  Leadership and Organizational Issues.						
+				2422, (21),  Administration and business development.						
+				2423, (65),  Personnel administration b. and career planners						
+				2424, (7),  Human Resources Development and trainers						
+			243, (324),  Sales				Sell	High Speaking		
+				2431, (288),  Specialists in advertising and marketing						
+				2432, (19),  spokespersons						
+				2433, (4),  Sales Engineers and Pharmaceutical Rapporteurs (eg ICT)						
+				2434, (16),  Sales of information and communication technology.						
+		25, (531),  Specialist in Information and Communication Technologies					Technical skills	High level intelligence	Computer	
+		26, (370),  Loans								
+			261, (29),  Specialists in finance				High level intelligence			
+				2611, (16),  Attorneys						
+				2612, (2),  Judicial Trustees						
+				2619, (11),  Other legal specialists						
+			262, (16),  Librarians and Archives Managers				High level intelligence			
+				2621, (5),  Archives Managers and Museum Specialists						
+				2622, (11),  Libraries						
+			263, (222),  Social and social affairs and religions.				High Speaking			
+			264, (62),  Reporters				High Speaking			
+				2643, (58),  Translators			High level intelligence			
+			265, (41),  Artists							
+	3, (2887),  Experts									
+		31, (448),  Science and Technology Specialists					Technical skills	High level intelligence	Higher Education	
+			311, (188),  Physics							
+				3111, (1),  Technical experts in natural sciences						
+				3112, (35),  Construction experts						
+				3113, (20),  Electrical engineers						
+				3114, (13),  Electronics specialists						
+				3115, (68),  Mechanical engineering specialists						
+				3116, (4),  Chemical Process Engineering Specialists						
+				3117, (1),  Mining and Metallurgy Specialists						
+				3118, (19),  Technical Drawers						
+				3119, (43),  Other physics						
+			312, (235),  Employee in mining							
+				3122, (77),  Industrial Executives						
+				3123, (159),  Construction Managers						
+			313, (11),  Process Control Experts							
+			314, (17),  Biosciences experts							
+				3141, (17),  Laboratori et al.						
+			315, (6),  Ship							
+				3151, (5),  Ship engineers and engineers						
+		32, (530),  Health Care Specialists								
+			321, (73),  Healthcare Technical Experts				Health care	High level intelligence	Higher Education	Low Speaking
+				3211, (15),  Medical Imaging and Apparatus a.						
+				3212, (29),  Bioanalyths (health care)						
+				3213, (28),  Pharmaceutics						
+			322, (311),  nurses				Low Speaking	Help others	Health	
+			325, (144),  Other health professionals				Health care	High level intelligence	Higher Education	Low Speaking
+				3251, (21),  Dental hygienists						
+				3254, (4),  Opticians						
+				3255, (66),  Physiotherapists and others.						
+				3257, (4),  Health and safety inspectors						
+				3259, (54),  Other unclassified health care a.						
+		33, (1381),  Business and Administration Specialists								
+			331, (144),  Financial				Technical skills	Higher Education	High level intelligence	
+				3313, (138),  Accounting and Accounting Specialists						
+			332, (929),  Sales and Purchasing Agents				High Speaking	Sell		
+				3321, (9),  Insurance Service Providers						
+				3322, (871),  Sales Representatives						
+				3323, (50),  Entrants						
+				3324, (3),  Traders						
+			333, (157),  Business Services Brokers				Technical skills	High Speaking		
+				3333, (27),  Employers						
+				3334, (98),  Real Estate Agents and Property Managers						
+				3339, (27),  Other business experts						
+			334, (107),  Administrative and specialized secretaries				Technical skills	High Speaking	Computer	
+				3341, (22),  Office Workers						
+				3342, (5),  Attorneys at law						
+				3343, (67),  Executive Secretaries and Section Titles						
+				3344, (13),  Office Secretaries (Health Care)						
+			335, (54),  Public Administration preparatory and supervisory officers				Technical skills	Higher Education	High Speaking	High level intelligence
+				3353, (13),  Social security benefits handlers						
+				3355, (6),  Commissaries and Overclocking Games						
+				3359, (35),  Other public administration preparatory and supervisory bodies						
+		34, (483),  Lawyer and Social and Cultural Affairs a.								
+			341, (310),  Law Enforcement a. And social welfare				High level intelligence	High Speaking	Higher Education	
+				3411, (24),  Loan Assistants and Associate Professor						
+				3412, (270),  Social counselors and advisors						
+				3413, (17),  Church workers						
+			342, (130),  Athletes				Walking			
+			343, (45),  Arts and Culture							
+				3434, (36),  Kitchen managers			Health			
+		35, (62),  Information and Communication Technology a.					Computer	High level intelligence		
+			351, (58),  ICT technicians and support staff							
+			352, (6),  telecommunications							
+	4, (640),  Office and customer service workers									
+		41, (202),  Office workers				Computer	High Speaking			
+			411, (13),  Office Assistants							
+			412, (186),  General Secretaries							
+			413, (4),  Text Handlers and Recorders							
+		42, (233),  Customer Service Officers				Help others	High Speaking			
+			421, (29),  Customer service workers in financial services							
+				4211, (17),  Banking and other employees						
+				4212, (11),  bookmakers						
+			422, (205),  Other customer service workers							
+				4221, (3),  Travel agency officers						
+				4222, (98),  Telephone Service Providers						
+				4223, (4),  Telephone exchangers						
+				4224, (32),  Hotel receptionists						
+				4225, (17),  Customer Information Officers at the Information Point						
+				4226, (28),  Reception and counseling nurses						
+				4227, (24),  Research and Market Research Interviews						
+		43, (119),  Office Workers in Computing and Storage				Low Speaking				
+			431, (82),  Payers			High level intelligence	Computer			
+				4311, (50),  Financial Officers						
+				4312, (4),  Office for statistical						
+				4313, (29),  Payers						
+			432, (37),  Office workers for transport and storage							
+				4321, (20),  Stockholders etc.						
+				4322, (8),  Production preparers						
+				4323, (12),  Office workers in transport						
+		44, (91),  Other office and customer service workers				High Speaking				
+			441, (91),  Other office and customer service workers							
+				4411, (2),  Library Workers						
+				4412, (57),  posters and sorters						
+				4415, (2),  Archivists						
+				4416, (23),  Assistant Office Employees for Human Resources						
+				4419, (7),  Other Classifieds. office and customer service						
+	5, (4577),  Service and sales workers									
+		51, (1640),  Service Workers								
+			511, (21),  Travel Service Workers				High Speaking	High level intelligence		
+				5113, (20),  Travel Guides						
+			512, (579),  Restaurant and large-scale workers				Health			
+				5120, (579),  Restaurant and large-scale workers						
+			513, (376),  Serving Workers							
+				5131, (352),  Waitresses			Walking	High Speaking	Low Skills	
+				5132, (27),  Barsmasters			Health	Low Speaking		
+			514, (363),  hairdressers				Work with hands			
+				5141, (254),  Hairdressers and shavers						
+				5142, (122),  cosmetologists and others.				Health care		
+			515, (281),  Managers of Real Estate Maintenance and Cleaning							
+				5151, (48),  Officers' Offices				High Speaking		
+				5153, (235),  Real Estate Maintenance Workers				Cleaning	Low Skills	
+			516, (29),  Other personal service employees							
+				5164, (17),  Animal husbandry and pet grooming				Animals	Low Speaking	
+				5165, (12),  Driving Instructors				High Speaking	High level intelligence	Cars
+		52, (1549),  Sellers						Sell	Low Speaking	
+			521, (43),  Street and street traders					Low Skills		
+			522, (959),  Sellers and Traders							
+				5221, (104),  Merchants (small entrepreneurs)				High Speaking		
+				5222, (49),  Retailers				High Speaking		
+				5223, (812),  Sellers						
+			523, (69),  Cashiers and Ticket Dealers							
+				5230, (69),  Cashiers and Ticket Dealers				Low Skills		
+			524, (503),  Other sales personnel							
+				5241, (2),  Model				High Speaking		
+				5242, (41),  Product rapporteurs				High Speaking		
+				5243, (54),  Direct Sellers				High Speaking		
+				5244, (173),  Sellers of Telephone and Customer Service Centers				High Speaking		
+				5245, (27),  Service Workers						
+				5246, (96),  Caf?s and bars						
+				5249, (135),  Other unclassified sales staff						
+		53, (1293),  Nursing and healthcare workers						Health care	Help others	Low Speaking
+		54, (122),  Protection and security workers						Help others	Low Speaking	
+	6, (67),  Farmers							Work with hands		
+		61, (53),  Farmers and Animal Growers						Animals		
+		62, (14),  Forestry and Fisheries Employees								
+	7, (2598),  Building									
+		71, (1223),  Construction workers and others (eg electricians)						Work with hands		
+			711, (715),  Construction Workers							
+			712, (433),  Building Finishing Workers							
+			713, (86),  Painters and Building Cleaners					Low Skills		
+		72, (927),  Workshop and foundry work						Technical skills	Work with mechanics	
+			721, (294),  Foundry workers							
+			722, (254),  Septi							
+			723, (390),  Machine installers and repairers							
+		73, (19),  Handicrafts Manufacturers						Work with hands		
+		74, (355),  Electrical and Electronics Employees						Technical skills	Work with hands	
+		75, (99),  Food						Health		
+			751, (40),  Meat clippers					Work with hands		
+			752, (28),  Timber handlers							
+				7522, (15),  Furniture fittings and so on.				Technical skills		
+				7523, (15),  Machine washers				Cleaning	Low Skills	
+			753, (20),  Clothing industry employees					Work with hands		
+	8, (1110),  Process and transport workers									
+		81, (235),  Process workers						Work with mechanics	Work with hands	
+			811, (43),  Mining and quarrying machinery					Technical skills		
+			814, (32),  Manufacture of rubber							
+			815, (34),  Textiles							
+				8157, (21),  Laundry workers				Cleaning		
+			816, (59),  Process workers in the food processing industry					Health		
+				8160, (59),  Process workers in the food processing industry						
+			817, (30),  Manufacture of sawn timber							
+				8172, (29),  Workers in wood and sawn goods						
+		82, (206),  Manufacturers of industrial products						Work with mechanics	Work with hands	
+			821, (206),  Manufacturers of industrial products							
+				8211, (145),  Manufacturers of hardware and metal products						
+				8212, (43),  Electrical and electronic equipment assemblers						
+				8219, (19),  Other assemblers of industrial products						
+		83, (673),  Transport workers						Cars		
+			832, (154),  Passenger cars					Low Speaking		
+			833, (335),  Heavy vehicle drivers							
+				8331, (34),  bus and tram drivers				Low Speaking		
+				8332, (301),  Truck and Special Vehicle Drivers						
+			834, (185),  Work machine drivers							
+			835, (5),  Crew and other waterborne workers							
+	9, (1828),  Other employees									
+		91, (830),  Cleaners						Cleaning	Low Skills	
+			911, (806),  Home							
+				9111, (90),  Homeowners and Excavators						
+				9112, (718),  Office and institutional detergents						
+			912, (24),  Vehicle cleaners							
+				9122, (22),  Vehicle washers						
+		93, (476),  Industrial and construction auxiliary workers								
+			931, (232),  Assistive Mining and Construction Workers					Work with mechanics		
+				9312, (114),  Assisting civil engineering						
+				9313, (119),  Assistants in the construction industry						
+			932, (15),  Manufacturing assistants					Work with hands		
+				9321, (12),  Handcuffs						
+				9329, (3),  Other assistants in manufacturing						
+			933, (229),  freight forwarders and warehouse workers					Cars		
+				9332, (3),  Drivers of animal driven vehicles						
+				9333, (222),  freight forwarders						
+				9334, (4),  Shelf fillers						
+		94, (372),  Assisting Kitchen and Food Workers								
+			941, (372),  Assistant kitchen and food workers					Health		
+				9411, (73),  Fast Food Workers				Low Speaking	Low Skills	
+				9412, (302),  Assistive kitchen workers					Low Skills	
+		95, (39),  street vendors						Walking	Low Skills	
+		96, (112),  Employees of street clean-up and waste management								
+			961, (11),  Waste Management Workers					Cleaning	Low Skills	
+			962, (101),  Newspaper distributors					Walking	Low Skills	
+EOF;
+
+define('SKILL_CLEANING',       1);
+define('SKILL_WALKING',        2);
+define('SKILL_HEALTH',         3);
+define('SKILL_CARS',           4);
+define('SKILL_HANDS',          5);
+define('SKILL_TECHNICAL',      6);
+define('SKILL_HEALTH_CARE',    7);
+define('SKILL_HELP',           8);
+define('SKILL_ANIMALS',        9);
+define('SKILL_SALES',         10);
+define('SKILL_LOW_SPEAKING',  11);
+define('SKILL_HIGH_SPEAKING', 12);
+define('SKILL_INTELLIGENCE',  13); //zero for Anton
+define('SKILL_COMPUTER',      14);
+define('SKILL_EDUCATION',     15);
+define('SKILL_MECHANICS',     16);
+define('SKILL_LOW',           17);
+
+$mapping = array(
+  'Technical skills' => 'SKILL_TECHNICAL',
+  'Higher Education' => 'SKILL_EDUCATION',
+   'Low Speaking' => 'SKILL_LOW_SPEAKING',
+   'Health care' => 'SKILL_HEALTH_CARE',
+   'High Speaking' => 'SKILL_HIGH_SPEAKING',
+   'Animals' => 'SKILL_ANIMALS',
+   'High level intelligence' => 'SKILL_INTELLIGENCE',
+   'Help others' => 'SKILL_HELP',
+   'Sell' => 'SKILL_SALES',
+   'Computer' => 'SKILL_COMPUTER',
+   'Health' => 'SKILL_HEALTH',
+   'Walking' => 'SKILL_WALKING',
+   'Low Skills' => 'SKILL_LOW',
+   'Work with hands' => 'SKILL_HANDS',
+   'Cleaning' => 'SKILL_CLEANING',
+   'Cars' => 'SKILL_CARS',
+   'Work with mechanics' => 'SKILL_MECHANICS',
+);
+
+$separated = explode("\n", $info);
+
+$built = array();
+
+
+foreach ($separated as $line) {
+  $tabbed = explode("\t", $line);
+  $skills = array();
+  $index = 0;
+  $category_id = '';
+  foreach ($tabbed as $item) {
+    if (!$item && !$index) continue;
+    if (!$item) continue;
+
+    if ($index == 0) {
+      list($category_id) = explode(',', $item);
+    } else {
+      $skills[$item] = true;
+    }
+
+    $index++;
+  }
+
+  if (strlen($category_id) > 1) {
+    $parent_id = substr($category_id, 0, strlen($category_id) - 1);
+    $built[$parent_id]['children'][] = $category_id;
+    $parent = $built[$parent_id];
+    $skills = array_merge($skills, $parent['skills']);
+  }
+
+  $category = array('id' => $category_id, 'skills' => $skills, 'children' => array());
+  $built[$category_id] = $category;
+}
+
+foreach ($built as $category_id => $info) {
+  if (!empty($info['children'])) continue;
+  echo $category_id . ' => array(';
+  foreach ($info['skills'] as $skill => $_) {
+    echo $mapping[$skill];
+    echo  ', ';
+  }
+  echo "), \n";
+}
