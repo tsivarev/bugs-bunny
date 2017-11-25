8a89b686a12d2892aeb5c071634cbfc0bc4ac51e
Anton Tsivarev <tsivarev.a@gmail.com>
2017-11-25 22:39:02 +0200

    distance

--- libs/logic.lib.php
+++ libs/logic.lib.php
@@ -57,14 +57,9 @@ function logic_wrapCard($step, $card, $lang) {
 
 function logic_getJobs($session_id, $lang, $skills, $categories) {
   $weights = weightCategories($skills, $categories);
-  $category_ids = array();
-  foreach ($weights as $category_id => $value) {
-    if ($value <= 0) {
-      continue;
-    }
-
-    $category_ids[$category_id] = $value;
-  }
+  $category_ids = array_filter($weights, function ($v) {
+    return $v >= 0;
+  });
 
   $result = db_query('SELECT * from JOBS_CATEGORIES where CATEGORY_ID IN ('.implode(',', array_keys($category_ids)).')');
 
@@ -85,11 +80,11 @@ function logic_getJobs($session_id, $lang, $skills, $categories) {
 
     $found = array_slice($job_category_ids[$category_id], 0, 3);
     foreach ($found as $job_id) {
-      $job_ids[$job_id] = 1;
+      $job_ids[$job_id] = $value;
     }
   }
 
-  $job_ids = array_slice(array_keys($job_ids),0, 100);
+  $job_ids = array_keys($job_ids);
 
   $result = db_query('SELECT * from JOBS where ID IN ('.implode(',', $job_ids).')');
   $jobs = array();
@@ -104,7 +99,7 @@ function logic_getJobs($session_id, $lang, $skills, $categories) {
       continue;
     }
 
-    if (!isset($json['yhteystiedot'])) {
+    if (!isset($json['tyopaikanOsoite'])) {
       continue;
     }
 
@@ -112,17 +107,101 @@ function logic_getJobs($session_id, $lang, $skills, $categories) {
       continue;
     }
 
+    if (isset($json['sijainti'])) {
+      list($a, $b) = explode(',', $json['sijainti']);
+      $distance = distance($a, $b, 60.1811483, 24.8090845, "K") * 1000;
+      if ($distance < 500) {
+        $distance = '<500m';
+      } else {
+        if ($distance < 1000) {
+          $distance = '<1km';
+        } else {
+          if ($distance < 3000) {
+            $distance = '<3km';
+          } else {
+            if ($distance < 5000) {
+              $distance = '<5km';
+            } else {
+              if ($distance < 100000) {
+                $distance = '<100km';
+              } else {
+                $distance = '>100km';
+              }
+            }
+          }
+        }
+      }
+    } else if (isset($json['postitoimipaikka'])){
+      $distance = ucfirst($json['postitoimipaikka']);
+    } else {
+      $distance = '';
+    }
 
     $jobs[] = array(
       'id'        => $json['ilmoitusnumero'],
       'title'     => translate_query($lang, $json['tehtavanimi'], 'fi'),
       'salary'    => translate_query($lang, $json['palkkausteksti'], 'fi'),
-      'address'   => str_replace('\r\n', ' ', $json['yhteystiedot']),
+      'address'   => str_replace('\r\n', ' ', $json['tyopaikanOsoite']),
+      'distance'  => $distance,
       'work_time' => translate_query($lang, $json['tyoaikatekstiYhdistetty'], 'fi'),
     );
   }
+
+  $jobs = array_slice($jobs,0, 30);
 
 
   return $jobs;
 }
 
+function haversineGreatCircleDistance(
+  $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
+{
+  // convert from degrees to radians
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+
+  $latDelta = $latTo - $latFrom;
+  $lonDelta = $lonTo - $lonFrom;
+
+  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+      cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+  return $angle * $earthRadius;
+}
+
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  $unit = strtoupper($unit);
+
+  if ($unit == "K") {
+    return ($miles * 1.609344);
+  } else if ($unit == "N") {
+    return ($miles * 0.8684);
+  } else {
+    return $miles;
+  }
+}
+
+function vincentyGreatCircleDistance(
+  $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
+{
+  // convert from degrees to radians
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+
+  $lonDelta = $lonTo - $lonFrom;
+  $a = pow(cos($latTo) * sin($lonDelta), 2) +
+    pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+  $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+  $angle = atan2(sqrt($a), $b);
+  return $angle * $earthRadius;
+}
