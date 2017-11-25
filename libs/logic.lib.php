db77863a3a56aad8c1c65a3ef5b7da692a05a36e
Anton Tsivarev <tsivarev.a@gmail.com>
2017-11-25 17:00:59 +0200

    Static

--- libs/logic.lib.php
+++ libs/logic.lib.php
@@ -53,64 +53,6 @@ function logic_wrapCard($step, $card, $lang) {
   );
 }
 
-function logic_getCards($session_id, $lang) {
-  $items = array();
-
-  $items[] = array(
-    'id'        => 6,
-    'text'      => 'wow',
-    'image_url' => 'https://pbs.twimg.com/profile_images/784791896405270530/CMKZpIpw.jpg'
-  );
-  $items[] = array(
-    'id'        => 6,
-    'text'      => 'wow',
-    'image_url' => 'https://pbs.twimg.com/profile_images/689152280818483200/17UIv0z8.png'
-  );
-  $items[] = array(
-    'id'        => 6,
-    'text'      => 'wow',
-    'image_url' => 'https://pbs.twimg.com/profile_images/880504880758005760/IWZAANG8.jpg'
-  );
-  $items[] = array(
-    'id'        => 6,
-    'text'      => 'wow',
-    'image_url' => 'https://pp.userapi.com/c639124/v639124562/5fb90/159RWfhK49w.jpg'
-  );
-  $items[] = array(
-    'id'   => 1,
-    'text' => 'driver'
-  );
-  $items[] = array(
-    'id'   => 2,
-    'text' => 'teacher'
-  );
-  $items[] = array(
-    'id'        => 6,
-    'text'      => 'wow',
-    'image_url' => 'https://pp.userapi.com/c639124/v639124562/5fb90/159RWfhK49w.jpg'
-  );
-  $items[] = array(
-    'id'   => 3,
-    'text' => 'lol'
-  );
-  $items[] = array(
-    'id'   => 4,
-    'text' => 'kek'
-  );
-  $items[] = array(
-    'id'        => 5,
-    'image_url' => 'https://pp.userapi.com/c639124/v639124562/5fb90/159RWfhK49w.jpg'
-  );
-
-  foreach ($items as $index => $item) {
-    if ($item['text']) {
-      $items[$index]['text'] = translate_query($lang, $item['text']);
-    }
-  }
-
-  return $items;
-}
-
 function logic_getJobs($session_id) {
   $result = db_query('SELECT * from JOBS limit 0, 10;');
   $jobs = array();
